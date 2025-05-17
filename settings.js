de6bd5460d011d2313fc255ad6ca85305617c7e5
Gistmedia <143992243+Gistmedia@users.noreply.github.com>
2025-05-17 13:09:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || 'KANGO~DEkkSJAJ#B7WdY9kNmvgm_TPEY6ePAAC-VcIQf7IVo-Y8Yy8MsVU' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~VitRlQhZ#93QwjOZt8ie_sN96P7rFkgIhi9siOuT6txmtWzgdsg8' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
